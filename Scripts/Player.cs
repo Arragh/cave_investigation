8dd5b863ef4888cbe0d1bee5ce7cfba4ad79af8d
Sergei <arragh1986@icloud.com>
2025-08-27 00:48:39 +0300

    Initial Commit

---
+++ Scripts/Player.cs
@@ -0,0 +1,48 @@
+using Godot;
+
+public partial class Player : CharacterBody2D
+{
+	[Export]
+	public AnimatedSprite2D IdleAnimation { get; set; }
+
+	[Export]
+	public AnimatedSprite2D RunAnimation { get; set; }
+
+	[Export]
+	public AnimatedSprite2D AttackAnimation { get; set; }
+
+	public const float Speed = 300.0f;
+	public const float JumpVelocity = -400.0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Input.GetVector("walk_left", "walk_right", "walk_up", "walk_down");
+		if (direction != Vector2.Zero)
+		{
+			velocity.X = direction.X * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
