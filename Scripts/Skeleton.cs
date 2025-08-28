b3835e5bf656a67a87b215bced03f8f43ec00e20
Sergei <arragh1986@icloud.com>
2025-08-28 22:45:20 +0300

    feat: начат скелет

--- Scripts/Skeleton.cs
+++ Scripts/Skeleton.cs
@@ -2,12 +2,29 @@ using Godot;
 
 public partial class Skeleton : CharacterBody2D
 {
-	[Export]
-	public AnimatedSprite2D Animation { get; set; }
+	private Vector2 _velocity = Vector2.Zero;
+	private float _direction = 1;
 
+	[Export]
+	public AnimatedSprite2D AnimatedSprite2D { get; set; }
+
+	[Export]
+	public int Speed = 100;
+
+	[Export]
+	public int Gravity = 2000;
+
+	public override void _Ready()
+	{
+		AnimatedSprite2D.Play("walk");
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
+		_velocity.X = _direction * Speed;
+		_velocity.Y += Gravity * (float)delta;
 
+		Velocity = _velocity;
+		MoveAndSlide();
 	}
 }
